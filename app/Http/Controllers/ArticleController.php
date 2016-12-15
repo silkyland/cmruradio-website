6cbfde289f7c134facf88ce3b152f669fe9daae6
Bundit Nuntates <silkyland@gmail.com>
2016-12-15 13:22:53 +0700

    Add ArticleController, BackendController

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ArticleController extends Controller
+{
+    // Admin Section
+    public function articles()
+    {
+        return view('admin.article');
+    }
+}
